85c55787de9f3ce1bda8058bece9dc8c6e433a04
Lucas Murakami <lucaskaitomurakami@gmail.com>
2023-07-10 21:24:56 -0300

    Projeto Iniciado

---
+++ src/test/java/com/example/dslistbackend/DslistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dslistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
