b56fc0954ca5bbf7b0db7fcbf51d15c69780e5e3
Lucas Murakami <lucaskaitomurakami@gmail.com>
2023-07-15 22:44:33 -0300

    Move games in the list funcion

--- src/main/java/com/example/dslistbackend/services/GameListService.java
+++ src/main/java/com/example/dslistbackend/services/GameListService.java
@@ -1,8 +1,11 @@
 package com.example.dslistbackend.services;
 
 import com.example.dslistbackend.dto.GameListDTO;
+import com.example.dslistbackend.entities.Game;
 import com.example.dslistbackend.entities.GameList;
+import com.example.dslistbackend.projections.GameMinProjection;
 import com.example.dslistbackend.repositories.GameListRepository;
+import com.example.dslistbackend.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,12 +16,32 @@ import java.util.List;
 public class GameListService {
 
     @Autowired
-    private GameListRepository gameRepository;
+    private GameListRepository gameListRepository;
+
+    @Autowired
+    private GameRepository gameRepository;
 
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
-        List<GameList> result = gameRepository.findAll();
+        List<GameList> result = gameListRepository.findAll();
         List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
         return dto;
     }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
+
+    }
+
 }
